39f2415fc211f41764fd9698709d9b0194601812
malathisankar <110773069+malathisankar@users.noreply.github.com>
2024-10-04 13:31:06 +0530

    Add files via upload

---
+++ mini_shell1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>     
+#include <stdlib.h>    
+#include <unistd.h>     
+#include <sys/types.h>  
+#include <string.h>
+
+int main() 
+{
+    char command[256]; 
+    char *args[10];    
+
+    while (1) 
+    {
+        printf("simple-shell> ");
+        fgets(command, 256, stdin); 
+
+        command[strcspn(command, "\n")] = 0;
+
+        int i = 0;
+        args[i] = strtok(command, " ");
+        while (args[i] != NULL) 
+        {
+            i++;
+            args[i] = strtok(NULL, " ");
+        }
+
+        if (strcmp(args[0], "exit") == 0) 
+        {
+            break;
+        }
+
+        if (strcmp(args[0], "cd") == 0) 
+        {
+            if (i < 2) 
+            {
+                fprintf(stderr, "cd: missing argument\n");
+            }
+            else 
+            {
+                if (chdir(args[1]) != 0) 
+                {
+                    perror("cd failed");
+                }
+            }
+            continue; 
+        }
+
+        if (system(command) == -1) 
+        {
+            fprintf(stderr, "Command execution failed\n");
+        }
+    }
+}
+
